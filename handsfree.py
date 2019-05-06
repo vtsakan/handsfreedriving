c8003de691d84960e3516764384672d3adfc0b7e
vtsakan <vasilistsakanikas@gmail.com>
2019-05-06 11:25:45 +0300

    Add files via upload

---
+++ handsfree.py
@@ -0,0 +1,151 @@
+import RPi.GPIO as GPIO
+import time
+import speech_recognition as sr
+
+class AlphaBot2(object):
+	
+	def __init__(self,ain1=12,ain2=13,ena=6,bin1=20,bin2=21,enb=26):
+		self.AIN1 = ain1
+		self.AIN2 = ain2
+		self.BIN1 = bin1
+		self.BIN2 = bin2
+		self.ENA = ena
+		self.ENB = enb
+		self.PA  = 30
+		self.PB  = 30
+
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setwarnings(False)
+		GPIO.setup(self.AIN1,GPIO.OUT)
+		GPIO.setup(self.AIN2,GPIO.OUT)
+		GPIO.setup(self.BIN1,GPIO.OUT)
+		GPIO.setup(self.BIN2,GPIO.OUT)
+		GPIO.setup(self.ENA,GPIO.OUT)
+		GPIO.setup(self.ENB,GPIO.OUT)
+		self.PWMA = GPIO.PWM(self.ENA,500)
+		self.PWMB = GPIO.PWM(self.ENB,500)
+		self.PWMA.start(self.PA)
+		self.PWMB.start(self.PB)
+		self.stop()
+
+	def forward(self):
+		self.PWMA.ChangeDutyCycle(20)
+		self.PWMB.ChangeDutyCycle(20)
+		GPIO.output(self.AIN1,GPIO.LOW)
+		GPIO.output(self.AIN2,GPIO.HIGH)
+		GPIO.output(self.BIN1,GPIO.LOW)
+		GPIO.output(self.BIN2,GPIO.HIGH)
+
+
+	def stop(self):
+		self.PWMA.ChangeDutyCycle(0)
+		self.PWMB.ChangeDutyCycle(0)
+		GPIO.output(self.AIN1,GPIO.LOW)
+		GPIO.output(self.AIN2,GPIO.LOW)
+		GPIO.output(self.BIN1,GPIO.LOW)
+		GPIO.output(self.BIN2,GPIO.LOW)
+
+	def backward(self):
+		self.PWMA.ChangeDutyCycle(20)
+		self.PWMB.ChangeDutyCycle(20)
+		GPIO.output(self.AIN1,GPIO.HIGH)
+		GPIO.output(self.AIN2,GPIO.LOW)
+		GPIO.output(self.BIN1,GPIO.HIGH)
+		GPIO.output(self.BIN2,GPIO.LOW)
+
+		
+	def left(self):
+		self.PWMA.ChangeDutyCycle(20)
+		self.PWMB.ChangeDutyCycle(20)
+		GPIO.output(self.AIN1,GPIO.HIGH)
+		GPIO.output(self.AIN2,GPIO.LOW)
+		GPIO.output(self.BIN1,GPIO.LOW)
+		GPIO.output(self.BIN2,GPIO.HIGH)
+
+
+	def right(self):
+		self.PWMA.ChangeDutyCycle(20)
+		self.PWMB.ChangeDutyCycle(20)
+		GPIO.output(self.AIN1,GPIO.LOW)
+		GPIO.output(self.AIN2,GPIO.HIGH)
+		GPIO.output(self.BIN1,GPIO.HIGH)
+		GPIO.output(self.BIN2,GPIO.LOW)
+		
+	def setPWMA(self,value):
+		self.PA = value
+		self.PWMA.ChangeDutyCycle(self.PA)
+
+	def setPWMB(self,value):
+		self.PB = value
+		self.PWMB.ChangeDutyCycle(self.PB)	
+		
+	def setMotor(self, left, right):
+		if((right >= 0) and (right <= 100)):
+			GPIO.output(self.AIN1,GPIO.HIGH)
+			GPIO.output(self.AIN2,GPIO.LOW)
+			self.PWMA.ChangeDutyCycle(right)
+		elif((right < 0) and (right >= -100)):
+			GPIO.output(self.AIN1,GPIO.LOW)
+			GPIO.output(self.AIN2,GPIO.HIGH)
+			self.PWMA.ChangeDutyCycle(0 - right)
+		if((left >= 0) and (left <= 100)):
+			GPIO.output(self.BIN1,GPIO.HIGH)
+			GPIO.output(self.BIN2,GPIO.LOW)
+			self.PWMB.ChangeDutyCycle(left)
+		elif((left < 0) and (left >= -100)):
+			GPIO.output(self.BIN1,GPIO.LOW)
+			GPIO.output(self.BIN2,GPIO.HIGH)
+			self.PWMB.ChangeDutyCycle(0 - left)
+
+if __name__=='__main__':
+	r = sr.Recognizer()
+	Ab = AlphaBot2()
+	while (True):
+		#Ab.forward();
+		#time.sleep(1);
+		#Ab.stop();
+		with sr.Microphone() as source:
+			print("Say something!")
+			audio = r.listen(source)
+
+	# recognize speech using Sphinx
+	
+	#	try:
+	#		print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+	#		Ab.forward();
+	#		time.sleep(1);
+	#		Ab.stop();
+	##	except sr.UnknownValueError:
+	#		print("Sphinx could not understand audio")
+	#	except sr.RequestError as e:
+	#		print("Sphinx error; {0}".format(e))
+	
+	# recognize speech using Google Speech Recognition
+			try:
+	    # for testing purposes, we're just using the default API key
+	    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+	    # instead of `r.recognize_google(audio)`
+				inputtext = r.recognize_google(audio);
+				print("Google Speech Recognition thinks you said " + inputtext);
+				if ('f'and 'w'and 'rd' in inputtext):
+					Ab.forward();
+					time.sleep(1);
+					Ab.stop();
+				elif ('r' and 'v' and 'r' and 's' in inputtext):
+					Ab.backward();
+					time.sleep(1);
+					Ab.stop();
+				elif ('l' and 'ft'  in inputtext):
+					Ab.left();
+					time.sleep(1);
+					Ab.stop();
+				elif ('right' in inputtext):
+					Ab.right();
+					time.sleep(1);
+					Ab.stop();
+			except sr.UnknownValueError:
+				print("Google Speech Recognition could not understand audio")
+			except sr.RequestError as e:
+				print("Could not request results from Google Speech Recognition service; {0}".format(e))
+	
+
